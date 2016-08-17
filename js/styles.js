292603b0cca4ea16bfc416495ba8dcdca8b87700
Kyle Fisher and Peter Armington <kyle@>
2016-08-17 10:36:51 -0400

    revised backend logic so that a correct result was able to be returned for all user input.

--- js/styles.js
+++ js/styles.js
@@ -1,35 +1,36 @@
+// Frontsies
+
 $(document).ready(function() {
     $("#inputForm").submit(function(event) {
       event.preventDefault();
 
       var year = $("#inputYear").val();
 
-      function leapYear(year) {
-        if (year % 4 === 0){
-          return true;
-        } else {
-          return false;
-        }
-      };
-
-      var yearBoolean = leapYear(year);
-
-      console.log(yearBoolean);
-    $(".determinalized").empty();
-
-      if(yearBoolean){
-        $(".determinalized").append(" a leap year.");
+      if (isNaN(year)) {
+        $(".determinalized").append(" not a numeric value. Please try again.")
       } else {
-        $(".determinalized").append(" not a leap year.")
+
+        var yearOutput = leapYear(year);
+
+        $(".determinalized").empty();
+
+        $(".determinalized").append(yearOutput);
       }
-
-
-
-
-
-
-
-
-
+    });
   });
-});
+
+// Backsies
+
+  function leapYear(year) {
+    if (year < -45) {
+      return " earlier than when leap years were established.";
+    } else if (year % 400 === 0) {
+      return " indeed a leap year!";
+    } else if (year % 100 === 0) {
+      return " not a leap year.";
+    } else if (year % 4 === 0) {
+      return " indeed a leap year!";
+    } else {
+      return " not a leap year.";
+    }
+  };
