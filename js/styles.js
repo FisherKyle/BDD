9a2d1b1be12add1a078511bacd5f6f7b1ed0d197
Kyle Fisher and Peter Armington <kyle@>
2016-08-17 09:54:09 -0400

    introduce initial backend logic capable of returning a result to the user.

--- js/styles.js
+++ js/styles.js
@@ -3,7 +3,27 @@ $(document).ready(function() {
       event.preventDefault();
 
       var year = $("#inputYear").val();
-  
+
+      function leapYear(year) {
+        if (year % 4 === 0){
+          return true;
+        } else {
+          return false;
+        }
+      };
+
+      var yearBoolean = leapYear(year);
+
+      console.log(yearBoolean);
+    $(".determinalized").empty();
+
+      if(yearBoolean){
+        $(".determinalized").append(" a leap year.");
+      } else {
+        $(".determinalized").append(" not a leap year.")
+      }
+
+
 
 
 
